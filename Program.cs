2e94131abb8493f4155a6831f7cfc7508ba86136
Kurt-22 <25-0227c@sgen.edu.ph>
2026-05-28 08:20:46 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,158 +4,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp17
+namespace ConsoleApp2
 {
-    class Rental
-    {
-        public string CustomerName { get; set; }
-        public string BookTitle { get; set; }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            string correctUsername = "admin";
-            string correctPassword = "1234";
+           
+            
+                // Create the dictionary
+                Dictionary<string, string> dictionary = new Dictionary<string, string>();
 
-            int attempts = 3;
-            bool isLoggedIn = false;
+                int choice = 0;
 
-            while (attempts > 0)
-            {
-                Console.Write("Enter username: ");
-                string username = Console.ReadLine();
-
-                Console.Write("Enter password: ");
-                string password = Console.ReadLine();
-
-                if (username == correctUsername && password == correctPassword)
+                // Loop until user chooses Exit
+                while (choice != 4)
                 {
-                    Console.WriteLine("\nLogin successful!\n");
-                    isLoggedIn = true;
-                    break;
-                }
-                else
-                {
-                    attempts--;
-                    Console.WriteLine($"Wrong credentials. Attempts left: {attempts}\n");
-                }
-            }
+                    Console.WriteLine("\n=====  KD's Knowledge =====");
+                    Console.WriteLine("1. Add Word");
+                    Console.WriteLine("2. Search Word");
+                    Console.WriteLine("3. Display All Words");
+                    Console.WriteLine("4. Exit");
+                    Console.Write("Enter your choice: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
 
-            if (!isLoggedIn)
-            {
-                Console.WriteLine("Too many failed attempts. Program will exit.");
-                return; 
-            }
+                    if (choice == 1)
+                    {
+                        Console.Write("Enter word: ");
+                        string word = Console.ReadLine();
 
-            Console.WriteLine("Welcome to the Library System!");
+                        Console.Write("Enter meaning: ");
+                        string meaning = Console.ReadLine();
 
-            Queue<Rental> rentalQueue = new Queue<Rental>();
-            List<Rental> borrowedBooks = new List<Rental>();
-            int choice;
-
-            do
-            {
-                Console.WriteLine("1. Add Customer");
-                Console.WriteLine("2. Serve Next Customer");
-                Console.WriteLine("3. View Queue");
-                Console.WriteLine("4. View Borrowed Books"); 
-                Console.WriteLine("5. Return Book");         
-                Console.WriteLine("6. Exit");
-                choice = int.Parse(Console.ReadLine());
-
-                switch (choice)
-                {
-                    case 1:
-                        Console.Write("Enter customer name: ");
-                        string name = Console.ReadLine();
-
-                        Console.Write("Enter book to borrow: ");
-                        string book = Console.ReadLine();
-
-                        rentalQueue.Enqueue(new Rental
+                        // Check if word already exists
+                        if (!dictionary.ContainsKey(word))
                         {
-                            CustomerName = name,
-                            BookTitle = book
-                        });
-
-                        Console.WriteLine("Customer added to queue!");
-                        break;
-
-                    case 2:
-                        if (rentalQueue.Count > 0)
-                        {
-                            Rental served = rentalQueue.Dequeue();
-                            borrowedBooks.Add(served); 
-                            Console.WriteLine($"\n{served.CustomerName} borrowed \"{served.BookTitle}\"");
+                            dictionary.Add(word, meaning);
+                            Console.WriteLine("Word added successfully!");
                         }
                         else
                         {
-                            Console.WriteLine("Queue is empty.");
+                            Console.WriteLine("Word already exists.");
                         }
-                        break;
+                    }
 
-                    case 3:
-                        Console.WriteLine("\nCustomers in Queue:");
-                        foreach (var r in rentalQueue)
+                    else if (choice == 2)
+                    {
+                        Console.Write("Enter word to search: ");
+                        string searchWord = Console.ReadLine();
+
+                        // Check if word exists
+                        if (dictionary.ContainsKey(searchWord))
                         {
-                            Console.WriteLine($"{r.CustomerName} → {r.BookTitle}");
-                        }
-                        break ; 
-                    
-                    
-
-                    case 4:
-                        Console.WriteLine("\nBorrowed Books:");
-
-                        if (borrowedBooks.Count == 0)
-                        {
-                            Console.WriteLine("No books are currently borrowed.");
+                            Console.WriteLine("Meaning: " + dictionary[searchWord]);
                         }
                         else
                         {
-                            foreach (var r in borrowedBooks)
+                            Console.WriteLine("Word not found.");
+                        }
+                    }
+
+                    else if (choice == 3)
+                    {
+                        Console.WriteLine("\n--- Dictionary Words ---");
+
+                        if (dictionary.Count == 0)
+                        {
+                            Console.WriteLine("Dictionary is empty.");
+                        }
+                        else
+                        {
+                           
+                            foreach (KeyValuePair<string, string> item in dictionary)
                             {
-                                Console.WriteLine($"{r.CustomerName} → {r.BookTitle}");
+                                Console.WriteLine(item.Key + " = " + item.Value);
                             }
                         }
-                        break;
+                    }
 
-                    case 5:
+                    else if (choice == 4)
+                    {
+                        Console.WriteLine("Program exited.");
+                    }
 
-                    
-                        Console.Write("Enter student name: ");
-                        string returnName = Console.ReadLine();
-
-                        Console.Write("Enter book to return: ");
-                        string returnBook = Console.ReadLine();
-
-                        Rental found = borrowedBooks.Find(r =>
-                            r.CustomerName.ToLower() == returnName.ToLower() &&
-                            r.BookTitle.ToLower() == returnBook.ToLower());
-
-                        if (found != null)
-                        {
-                            borrowedBooks.Remove(found);
-                            Console.WriteLine($"{returnName} returned \"{returnBook}\"");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Record not found. Check name or book.");
-                        }
-                        break;
-
+                    else
+                    {
+                        Console.WriteLine("Invalid choice. Try again.");
+                    }
                 }
-
-            } while (choice != 6);
-
-            Console.WriteLine("Program ended.");
+            
         }
+
+
+
+
+
     }
-
-
-
-}    
-    
+}
 
